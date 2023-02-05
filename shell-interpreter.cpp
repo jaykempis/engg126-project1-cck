75590db278151eebf63baca31b0c1bf63bd3d739
eyendsea <68234832+eyendsea@users.noreply.github.com>
2023-02-06 07:29:47 +0800

    - combined previous files + has get current dir

--- shell-interpreter.cpp
+++ shell-interpreter.cpp
@@ -1,10 +1,28 @@
-//PROJECT 1 - Rudimentary Shell Interpreter
-//by Iris Nicole Carson
+//ENGG 126
+//Project 1: Rudimentary Shell Interpreter
+//by: Iris Carson, Antonio Castro, and Joshua Kempis
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <fstream>
-
+#include <filesystem>
+#include <unistd.h>
+#include <windows.h>
+/*
+#include <vector>
+#include <direct.h>
+*/
 using namespace std;
+//using namespace std::filesystem;
+
+void helpCMD ()
+{
+  cout<<"\nLists of possible commands: \n"
+      <<"* help            - displays the lists of commands \n"
+      <<"* ls              - list all files and directories in the present working directory\n"
+      <<"* exit            - closes the program"<<endl;
+}
 
 void fileRead (string filename)
 {
@@ -29,21 +47,41 @@ void fileWrite (string filename, string line)
   file.close();
 }
 
-int main (int argc, char **argv)
+void getCurrDir (string &cDir)
 {
-  //for checking of user input
-  cout<<"No. of words in entered command: "<<argc
-      <<" w/ arguments:"<<endl;
-  for (int i=0; i < argc; i++)
-  {
-    cout << argv[i] <<endl;
-  }
-  
-  //testing the file functions
-  string line = "sample";
-  fileRead("trial.txt");
-  fileWrite("trial.txt", line);
-  fileRead("trial.txt");
+  char tmp[256];
+  getcwd (tmp, 256);
+  cout<<"Current working directory: "<< tmp << endl;
+  cDir = tmp;
+}
 
-return 0;
+int main()
+{
+  string cmd, dir;
+  getCurrDir(dir);
+  //cout<<dir<<endl;
+  /*
+  for (const auto & entry : std::filesystem::directory_iterator(dir))
+    std::cout << entry.dir() << endl;
+  */
+  do
+  {	
+    cout<<"\n"<<dir<<"\n[CMD] : ";
+    getline(cin, cmd);
+    if (cmd == "help")
+    {
+      helpCMD();
+    }
+    else if (cmd == "ls")
+    {
+      cout<<"still working on this function"<<endl;
+    }
+    else if (cmd == "exit")
+    {
+      cout << "Exiting..." << endl;
+    }
+    else cout<<"Invalid Command. " <<
+      "Type 'help' for a list of commands."<<endl;
+  }
+  while (cmd != "exit");
 }
