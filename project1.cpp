ad636abd179f552657f648850f83c9610555431e
jaykempis <jay.kempis@bolt.graphics>
2023-02-13 07:50:49 +0800

    Updated files

---
+++ project1.cpp
@@ -0,0 +1,45 @@
+//Iris Carson, Antonio Castro & Jay Kempis
+//ENGG 126: Project 1
+
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include<stdlib.h>
+
+using namespace std;
+
+//function to run a command using string 
+string exec(string command) {
+   char buffer[128];
+   string result = "";
+
+   // Open pipe to file
+   FILE* pipe = popen(command.c_str(), "r");
+   if (!pipe) {
+      return "popen failed!";
+   }
+
+   // read till end of process:
+   while (!feof(pipe)) {
+      // use buffer to read and add to result
+      if (fgets(buffer, 128, pipe) != NULL)
+         result += buffer;
+   }
+
+   pclose(pipe);
+   return result;
+}
+
+int main() {
+   
+   string command;
+   cout << "> ";
+   getline(cin,command);
+
+   cout << "Command: " << command << endl;
+
+   string content = exec(command);
+   cout << content;
+
+}
