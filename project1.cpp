857b840263db41ef3a4aab95bb1bb81d8d5c777c
AntonCastroOBF <113913974+AntonCastroOBF@users.noreply.github.com>
2023-02-19 18:27:55 +0800

    Fixed input and output redirection
    Also added some indicators

--- project1.cpp
+++ project1.cpp
@@ -1,46 +1,156 @@
 //ENGG 126
 //Project 1: Rudimentary Shell Interpreter
 //by: Iris Carson, Antonio Castro, and Joshua Kempis
-
+//--------------------------------------------------------------------
 #include <iostream>
-#include <stdexcept>
-#include <stdio.h>
 #include <string>
-#include<stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <vector>
+//#include <unistd.h>
+#include <stdexcept>
 
 using namespace std;
 
-//function to run a command using string 
-string exec(string command) {
-   char buffer[128];
-   string result = "";
+void fileRead (string filename, string &contents)
+{
+  string line;
+  ifstream file (filename);
 
-   // Open pipe to file
-   FILE* pipe = popen(command.c_str(), "r");
-   if (!pipe) {
-      return "popen failed!";
-   }
-
-   // read till end of process:
-   while (!feof(pipe)) {
-      // use buffer to read and add to result
-      if (fgets(buffer, 128, pipe) != NULL)
-         result += buffer;
-   }
-
-   pclose(pipe);
-   return result;
+  if (file.is_open())
+  {
+    while (getline (file, line))
+    {
+	  cout << line << endl;
+	  contents = contents + line + "\n";
+    }
+	file.close();
+  }
+  else cout <<"File does not exist"<<endl;
 }
 
-int main() {
-   
-   string command;
-   cout << "$ ";
-   getline(cin,command);
+void fileWrite (string filename, string line)
+{
+  ofstream file(filename);
+  file << line;
+  file.close();
+}
 
-   cout << "Command: " << command << endl;
+/*
+void getCurrDir (string &cDir)
+{
+  char tmp[256];
+  getcwd (tmp, 256);
+  //cout<<"Current working directory: "<< tmp << endl;
+  cDir = tmp;
+}*/
 
-   string content = exec(command);
-   cout << content;
+string exec(string command) 
+{
+  char buffer[128];
+  string result = "";
 
+  // Open pipe to file
+  FILE* pipe = _popen(command.c_str(), "r");
+  if (!pipe) {
+    return "popen failed!";
+  }
+  // read till end of process:
+  while (!feof(pipe))
+  {
+    // use buffer to read and add to result
+    if (fgets(buffer, 128, pipe) != NULL)
+      result += buffer;
+  }
+  _pclose(pipe);
+  return result;
+}
+
+void parsing(string cmd, vector <string> &comms, 
+             string &c1, string &c2, int &mode)
+{
+  stringstream ss(cmd);
+  string word;
+  comms.clear();
+  while (ss>>word)
+  {
+    comms.push_back(word); 
+  }
+  bool found = false;
+  for (int i = 0; i < comms.size(); i++)
+  {	
+    if (comms[i] == "|" || comms[i] == ">" || comms[i] == "<")
+	{ 
+      if (comms[i] == "|") mode = 1;
+	  else if (comms[i] == ">") mode = 2;
+	  else if (comms[i] == "<") mode = 3;
+	  
+	  found = true;
+      
+	  cout<<"found symbol:"<<comms[i]<<endl;
+      for (int j=0; j < i; j++)
+	  {
+        c1 = c1 + comms[j] + ' ';	  
+      }
+      //cout<<c1<<endl;
+	  
+	  for (int k = i+1; k < comms.size(); k++)
+	  {
+        c2 = c2 + comms[k] + ' ';	  
+      }
+      //cout<<c2<<endl;
+    }
+	if (found == false) mode = 0;
+  }
+}
+
+int main()
+{
+  string cmd, dir, command, c1, c2, content;
+  vector <string> commands;
+  int mode;
+  //getCurrDir(dir);
+  //cout<<dir<<endl;
+  do
+  {	
+    cout<<"\n"<<dir<<"\n[CMD] : ";
+    getline(cin, cmd);
+	parsing(cmd, commands, c1, c2, mode);
+	cout<<"MODE:"<<mode<<endl;
+	
+	if (mode == 1){
+	  cout<<"Entered mode 1"<<endl;
+      cout << "C1: " << c1 << endl;
+      cout << "C2: " << c2 << endl;
+      //string newcommand = 
+      exec(cmd);
+	}
+	else if (mode == 2){
+	  cout<<"Entered mode 2"<<endl;
+      content = exec(c1);
+	  fileWrite(c2, content);
+      cout << "C1: " << c1 << endl;
+      cout << "C2: " << c2 << endl;
+    }
+	else if (mode == 3){
+	  cout<<"Entered mode 3"<<endl;
+      fileRead(c2, content);
+      
+	  cout<< exec(c1 +" "+ content) <<endl;
+
+      cout << "C1: " << c1 << endl;
+      cout << "C2: " << c2 << endl;
+	}
+	else{
+	  cout<<"Entered mode 0"<<endl;
+	  content = exec(cmd);
+	  fileWrite("log.txt", content);
+      cout << content;
+      cout << "C1: " << c1 << endl;
+      cout << "C2: " << c2 << endl;
+    } 
+    c1.clear();
+    c2.clear();
+  }
+  while (cmd != "exit");
 }
