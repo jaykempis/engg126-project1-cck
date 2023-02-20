94f3dc6a1fc6e24885f67538bffd5bd64531b747
eyendsea <68234832+eyendsea@users.noreply.github.com>
2023-02-20 09:10:50 +0800

    - cleaned version

--- shell-proj.cpp
+++ shell-proj.cpp
@@ -27,15 +27,15 @@ void fileRead (string filename, string &contents)
 {
   string line;
   ifstream file (filename);
-    
+
   if (file.is_open())
   {
     while (getline (file, line))
     {
-	  cout << line << endl;
-	  contents = contents + line + "\n";
+      //cout << line << endl;
+      contents = contents + line + "\n";
     }
-	file.close();
+    file.close();
   }
   else 
   {
@@ -58,7 +58,9 @@ string exec(string command)
 
   // Open pipe to file
   FILE* pipe = popen(command.c_str(), "r");
-  if (!pipe) {
+  
+  if (!pipe)
+  {
     return "popen failed!";
   }
   // read till end of process:
@@ -68,6 +70,7 @@ string exec(string command)
     if (fgets(buffer, 128, pipe) != NULL)
       result += buffer;
   }
+  
   pclose(pipe);
   return result;
 }
@@ -82,110 +85,130 @@ void parsing(string cmd, vector <string> &comms,
   {
     comms.push_back(word); 
   }
+  
   bool found = false;
+  
   for (int i = 0; i < comms.size(); i++)
-  {	
+  {  
     if (comms[i] == "|" || comms[i] == ">" || comms[i] == "<")
-	{ 
-      if (comms[i] == "|") mode = 1;
-	  else if (comms[i] == ">") mode = 2;
-	  else if (comms[i] == "<") mode = 3;
-	  
-	  found = true;
-      
-	  cout<<"found symbol:"<<comms[i]<<endl;
+    { 
+      if (comms[i] == "|")
+	  { 
+        mode = 1;
+      }
+	  else if (comms[i] == ">") 
+	  { 
+        mode = 2;
+      }
+      else if (comms[i] == "<")  
+	  { 
+        mode = 3;
+      }
+
+      found = true;
+
       for (int j=0; j < i; j++)
-	  {
-        c1 = c1 + comms[j] + ' ';	  
+      {
+        c1 = c1 + comms[j] + ' ';    
       }
       //cout<<c1<<endl;
-	  
-	  for (int k = i+1; k < comms.size(); k++)
-	  {
-        c2 = c2 + comms[k] + ' ';	  
+
+      for (int k = i+1; k < comms.size(); k++)
+      {
+        c2 = c2 + comms[k] + ' ';    
       }
       //cout<<c2<<endl;
     }
-	if (found == false) mode = 0;
+    if (found == false) mode = 0;
   }
 }
 
 int pipeFunc(string cmd1, string cmd2, int mode) 
 {
-  cout<<"CMD1: "<<cmd1<<" == CMD2: "<<cmd2<<endl;
   int fd1[2]; // first pipe
   int fd2[2]; // second pipe
-  
+
   int pid;
   string temp;
-  
+
   if (pipe(fd1) < 0)
   {
     cout<<"Pipe Failed"<<endl;
-	return 1;
+    return 1;
   }
   if (pipe(fd1) < 0) 
   {
     cout<<"Pipe Failed"<<endl;
-	return 1;
+    return 1;
   }
-  
+
   pid = fork();
-  
-  if (mode == 3) fileRead(cmd2, temp);
-  else temp = exec(cmd1);
-  
+
+  if (mode == 3)
+  {
+    fileRead(cmd2, temp);
+  }
+  else 
+  {
+    temp = exec(cmd1);
+  }
+
   char inbuf[temp.length()];
   char outbuf[temp.length()];
-  
+
   if (pid < 0) 
   {
     cout<<"Fork Failed"<<endl;
-	return 1;
+    return 1;
   }
+  
   //Parent Process
   else if (pid > 0)
   {
-    cout<<"IN:"<<mode<<endl;
     close(fd1[0]); // Close reading end of first pipe
     // Write input string and close writing end of first pipe.
-	write(fd1[1], temp.c_str(), temp.length()+1);
-	close(fd1[1]);
-	
-	wait(NULL);
-	read(fd2[0], outbuf, temp.length());
-	if (mode == 3) fileWrite(cmd1, outbuf);
+    write(fd1[1], temp.c_str(), temp.length()+1);
+    close(fd1[1]);
+
+    wait(NULL);
+    read(fd2[0], outbuf, temp.length());
+    if (mode == 3) fileWrite(cmd2, outbuf);
+    close(fd2[0]);
   }
-  
+
   //Child process
   else
   {
-	cout<<"child process"<<endl;
-	// Read a string using first pipe
-	read(fd1[0], inbuf, temp.length());
-	inbuf[temp.length()] = '\0';
-	
-	if (mode == 2) fileWrite(cmd2, inbuf);
-	else fileWrite("log.txt", inbuf);
-	
-	if (mode == 1)
+    // Read a string using first pipe
+    read(fd1[0], inbuf, temp.length());
+    inbuf[temp.length()] = '\0';
+
+    fileWrite("log.txt", inbuf);
+
+    if (mode == 1)
     {
       temp = exec(cmd2 + " " + "log.txt");
-	  cout <<temp<<endl;
-	  write(fd2[1], temp.c_str(), temp.length()+1);
+      cout <<temp<<endl;
+      write(fd2[1], temp.c_str(), temp.length()+1);
     }
-	
-	else if (mode == 3)
+  
+    else if (mode == 2)
+    {
+      fileWrite(cmd2, inbuf); 
+      write(fd2[1], temp.c_str(), temp.length()+1);
+    }
+  
+    else if (mode == 3)
     {
       temp = exec(cmd1 + " " + "log.txt");
-	  write(fd2[1], temp.c_str(), temp.length());
+      cout<<temp<<endl;
+      write(fd2[1], temp.c_str(), temp.length());
     }
-	
-	//cout<<"BUF CONTENTS:\n"<<inbuf<<endl;
+
+    //cout<<"BUF CONTENTS:\n"<<inbuf<<endl;
     close(fd1[0]);
-    close(fd2[0]);
-	close(fd2[1]);
-	exit(0);
+    close(fd2[1]);
+    exit(0);
   }  
   return 0;
 }
@@ -199,19 +222,19 @@ int main()
   getCurrDir(dir);
   //cout<<dir<<endl;
   do
-  {	
+  {  
     cout<<"\n"<<dir<<"\n[CMD] : ";
     getline(cin, cmd);
-	parsing(cmd, commands, c1, c2, mode);
-	cout<<"MODE:"<<mode<<endl;
-	
-	if (mode == 0)
+    parsing(cmd, commands, c1, c2, mode);
+    cout<<"==========================="<<endl;
+
+    if (mode == 0)
     {
       content = exec(cmd);
       fileWrite("log.txt", content);
       cout << content;
     }
-	
+
     else
     {
       pipeFunc(c1, c2, mode);
